097ddd294a6144837e010ad70f8790e6ea1a49d2
Uday-sidagana <uday.sidgana@gmail.com>
2025-02-26 04:30:10 +0530

    Loaded .env from file inot the python script

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from google import genai
 
-client = genai.Client(api_key="GEMINI_API_KEY")
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+
+client = genai.Client(api_key="API_KEY")
 
 response = client.models.generate_content(
     model="gemini-2.0-flash",
