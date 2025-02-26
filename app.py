99734f927031c5d00293af03b5f09658e0c1c64e
Uday-sidagana <uday.sidgana@gmail.com>
2025-02-27 01:14:38 +0530

    If you want to use ChatGPT

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
-from google import genai
+# from google import genai
+
+from openai import OpenAI
 
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-api_key = os.getenv("API_KEY")
+api_key = os.getenv("GPT_KEY")
 
-client = genai.Client(api_key="API_KEY")
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents=["How does AI work?"])
-print(response.text)
+client = OpenAI(
+  api_key=api_key
+)
+
+completion = client.chat.completions.create(
+  model="gpt-4o-mini",
+  store=True,
+  messages=[
+    {"role": "user", "content": "Tell me how to make an AI Agent who can automatically fill a google form"}
+  ]
+)
+
+print(completion.choices[0].message)
